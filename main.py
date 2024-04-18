b2a6e7cfb9b7cc37dfdcb007e7335a5e78134f0b
mico005 <michaeldevon005@gmail.com>
2024-04-19 07:44:30 +0800

    init

---
+++ main.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import time
+
+st.set_page_config(page_title='Happy Birthday', layout='centered')
+
+st.header('Did I forgot your birthday?')
+
+if (st.button('Click me!') == True):
+    st.error('OFCOURSE NO!')
+    with st.spinner('Wait for it...'):
+        time.sleep(3)
+    st.balloons()
+    st.title('Happy Birthday, MILF!')
+    st.write(
+        'In these days you were born about 19 years ago! What an old lady you are lol.')
+    st.write(
+        "Even tho you're old, you have nice tits hehe.. and I love it soo much.")
+    st.image('img/DASH-1.jpg',
+             caption='Look at this girl, she was soo cute back then..')
+    st.write(
+        "So it's been like 4 years and yet we're still communicating through WhatsApp, occasionally, because you were sooo busy ;(")
+    st.write(
+        "It's okay, I understand that. Many things has been happened to me either.")
+    st.write('I am really wishing that we could met with eachother one day. It could be fun if we met hehe')
+    st.image('img/DASH-2.jpg', caption="I think this is the first photo you sent me. My honest first reaction is 'Damn this girl is badass.'")
+    st.write("This stuff was made like about 2 weeks ago. I've spent my week to make this. Fortunately it's holiday! so I could work on this.")
+    st.write("I am really sorry if this website become trash because I only got a week to work on it. This words was typed before I decorate the page, so I don't even know how it's gonna look, I hope it's alright and you like it.")
+    st.image('img/DASH-3.jpg',
+             caption="This is how mommy Dasha eats ? She is so greedy like she is greedy in the bed too 😜")
+    st.write(
+        "Thank you for accompanying me while it was covid time. I am soo childish back then..")
+    st.write(
+        "I dont even know what else I wanna talk to you. I am just wishing you all the best.")
+    st.write(
+        "I've got some funny pictures that I kept on my phone, soo here are some of it, also please read the captions.")
+    st.image('img/DASH-6.jpg', caption="I remembered you cried, I think this is when you break up with your boy? You look so dumb honestly, jk hehe")
+    st.image('img/DASH-7.jpg', caption="See, this is the funniest thing of you. You're soo silly. I also love this pic because I can see how big is ur bewbs is 😜 ")
+    st.image('img/DASH-8.jpg',
+             caption="This is the first 'hot' pic you sent to me. I know it's not soo hot but it's something for me back then.")
+    st.image('img/DASH-9.jpg', caption="Now this is one of your hot pic you sent to me. I honestly want to see you posing like this but doesnt wear anything tehee")
+    st.image('img/DASH-10.jpg', caption="")
+    st.image('img/DASH-4.JPEG', caption="POV mommy Dasha wants my D hehe")
+    st.image('img/DASH-5.jpg',
+             caption="Mommy Dasha looks soo hot when just wearing underwear. I cant imagine when she doesn't wear anything 🔥")
+
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
